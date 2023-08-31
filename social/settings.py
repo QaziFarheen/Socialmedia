57c73c9b70e80c41b10d92f0aa3b848a3a0723e6
QaziFarheen <qazifarhin78@gmail.com>
2023-08-31 06:59:41 -0600

    new

--- social/settings.py
+++ social/settings.py
@@ -79,10 +79,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
-
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
